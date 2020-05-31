19bc7fa4951ee9e059e3c762ef764a03154d93bc
roukell <62136980+roukell@users.noreply.github.com>
2020-05-31 18:39:52 +1000

    updated

--- Develop/script.js
+++ Develop/script.js
@@ -6,7 +6,7 @@ document.getElementById("generateBtn").addEventListener("click", function userIn
   const charNumber = confirm("Click Ok if you would like numbers in your password, if not click Cancel");
 
   let charLength = null;
-
+// create loop to prompt user to type in correct input(length of character)
   while (charLength == null) {
     let charLength = Number(prompt("How many character would you like to have in your password? (between 8 ~ 128 characters)"));
     if (charLength < 8 || charLength > 128 || charLength == null) {
@@ -19,14 +19,13 @@ document.getElementById("generateBtn").addEventListener("click", function userIn
   }
 });
 
-// function that generates random cases, numbers, and symbols
+// define functions that generate random cases, numbers, and symbols
 const randomFunc = {
   charLowercase: getRandomLower,
   charUppercase: getRandomUpper,
   charNumber: getRandomNumber,
   charSymbol: getRandomSymbol,
 };
-
 
 
 // Generate password function
@@ -39,7 +38,7 @@ function generatePassword(charLength, charLowercase, charUppercase, charSymbol, 
   const typesArray = [{charLowercase}, {charUppercase}, {charSymbol}, {charNumber}].filter(item => Object.values(item)[0]);
   console.log("typesArray: ", typesArray);
 
-  // 2. if user selected nothing, password will not generate
+  // 2. if user selected nothing, password will not generate and alert user
   if (typesCount === 0){alert ("Error! Please use at least one character");
     return "";
   }
@@ -47,11 +46,13 @@ function generatePassword(charLength, charLowercase, charUppercase, charSymbol, 
   // 3. loop over length call generator function for each type
   for (let i = 0; i <= charLength; i += typesCount) {
     typesArray.forEach(type => {const funcName = Object.keys(type)[0];
-      console.log('funcName: ', funcName);
+
+      // console.log('funcName: ', funcName);
 
     generatedPassword = generatedPassword + randomFunc[funcName]();
     });
   }
+  // 4. get final results (.slice chops off 1 extra character)
 const finalPassword = generatedPassword.slice(0, charLength);
 return finalPassword;
 }
