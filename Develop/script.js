7f285fb7c66928d0501f1fdd51ffa8cef87d4e31
roukell <62136980+roukell@users.noreply.github.com>
2020-05-31 14:50:54 +1000

    updated js

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,18 @@
 document.getElementById("generateBtn").addEventListener("click", function userInput() {
-  let lowercase = confirm("Click Ok if you would like lowercase in your password, if not click Cancel");
-  let uppercase = confirm("Click Ok if you would like lowercase in your password, if not click Cancel");
-  let symbol = confirm("Click Ok if you would like lowercase in your password, if not click Cancel");
+  const lowercase = confirm("Click Ok if you would like lowercase in your password, if not click Cancel");
+  const uppercase = confirm("Click Ok if you would like lowercase in your password, if not click Cancel");
+  const symbol = confirm("Click Ok if you would like lowercase in your password, if not click Cancel");
+
   let length = null;
 
   while (length == null) {
     let length = Number(prompt("How many character would you like to have in your password? (between 8 ~ 128 characters)"));
     if (length < 8 || length > 128 || length == null) {
       prompt("error, only 8~128 char, please retype.");
-    } else {
-      console.log(length);
+    } 
+    {
+      console.log(length, lowercase, uppercase, symbol);
       return length;
     }
   }
 });
-
-
