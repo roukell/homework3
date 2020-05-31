5b2eb75ab3aab885ffc16e02c7f864b64f7d37e4
roukell <62136980+roukell@users.noreply.github.com>
2020-05-31 15:19:41 +1000

    correct loop

--- Develop/script.js
+++ Develop/script.js
@@ -9,10 +9,11 @@ document.getElementById("generateBtn").addEventListener("click", function userIn
     let length = Number(prompt("How many character would you like to have in your password? (between 8 ~ 128 characters)"));
     if (length < 8 || length > 128 || length == null) {
       prompt("error, only 8~128 char, please retype.");
-    } 
+    } else
     {
       console.log(length, lowercase, uppercase, symbol);
       return length;
     }
   }
 });
+
