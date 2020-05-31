6ecd57f72a6a29ddfc8e58aff86713d8984bc63e
roukell <62136980+roukell@users.noreply.github.com>
2020-05-31 18:40:59 +1000

    updated final js code

--- Develop/script.js
+++ Develop/script.js
@@ -6,13 +6,13 @@ document.getElementById("generateBtn").addEventListener("click", function userIn
   const charNumber = confirm("Click Ok if you would like numbers in your password, if not click Cancel");
 
   let charLength = null;
-// create loop to prompt user to type in correct input(length of character)
+  // create loop to prompt user to type in correct input(length of character)
   while (charLength == null) {
     let charLength = Number(prompt("How many character would you like to have in your password? (between 8 ~ 128 characters)"));
     if (charLength < 8 || charLength > 128 || charLength == null) {
       prompt("Error! Only enter 8~128 characters, please reenter.");
-    } else
-    {
+    } else {
+      // link the final password to HTML
       password.innerText = generatePassword(charLength, charLowercase, charUppercase, charSymbol, charNumber);
       return charLength;
     }
@@ -29,32 +29,43 @@ const randomFunc = {
 
 
 // Generate password function
-function generatePassword(charLength, charLowercase, charUppercase, charSymbol, charNumber){
+function generatePassword(charLength, charLowercase, charUppercase, charSymbol, charNumber) {
   // 1. initial password variable
   let generatedPassword = "";
   const typesCount = charLowercase + charUppercase + charSymbol + charNumber;
   console.log("typesCount: ", typesCount);
+
+  const typesArray = [{
+    charLowercase
+  }, {
+    charUppercase
+  }, {
+    charSymbol
+  }, {
+    charNumber
+  }].filter(item => Object.values(item)[0]);
   
-  const typesArray = [{charLowercase}, {charUppercase}, {charSymbol}, {charNumber}].filter(item => Object.values(item)[0]);
-  console.log("typesArray: ", typesArray);
+  // console.log("typesArray: ", typesArray);
 
   // 2. if user selected nothing, password will not generate and alert user
-  if (typesCount === 0){alert ("Error! Please use at least one character");
+  if (typesCount === 0) {
+    alert("Error! Please use at least one character");
     return "";
   }
 
   // 3. loop over length call generator function for each type
   for (let i = 0; i <= charLength; i += typesCount) {
-    typesArray.forEach(type => {const funcName = Object.keys(type)[0];
+    typesArray.forEach(type => {
+      const funcName = Object.keys(type)[0];
 
       // console.log('funcName: ', funcName);
 
-    generatedPassword = generatedPassword + randomFunc[funcName]();
+      generatedPassword = generatedPassword + randomFunc[funcName]();
     });
   }
   // 4. get final results (.slice chops off 1 extra character)
-const finalPassword = generatedPassword.slice(0, charLength);
-return finalPassword;
+  const finalPassword = generatedPassword.slice(0, charLength);
+  return finalPassword;
 }
 
 
